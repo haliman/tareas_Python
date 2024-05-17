f97dd9f927e1a33c17475fceb1590a6185626d97
haliman <huesas@gmail.com>
2024-05-17 11:26:08 +0200

    V 1.1

--- tareas/controllers/tareasController.py
+++ tareas/controllers/tareasController.py
@@ -3,24 +3,58 @@ from colorama import Fore, Style
 from models.tarea import Tarea
 
 class TareasController:
+    '''
+        Clase TareasController donde están los módulos para realizar todas las gestiones con las tareas como agregar, mostrar.....
+        
+        Atributos: 
+            tareas(list): Lista de tareas donde se almacena las tareas.
+    '''
     def __init__(self):
+        #Inicializamos las tareas con una lista vacía.
         self.tareas = []
-    
+        
+    #Mostramos todas las tareas que hay en la lista.
     def mostarTareas(self):
         for i, tarea in enumerate(self.tareas):
             print(str(i+1)+'.',tarea)
             
+    '''
+        Agregamos una nueva tarea
+        
+        Parámetros:
+            descripción (str): descripción de la nueva tarea.
+    '''
     def agregarTarea(self, descripcion):
         self.tareas.append(Tarea(descripcion))
+        print('Se ha creado la tarea con exito!!')
+    
+    '''
+        Eliminar una tarea
         
+        Parámetros:
+            posición(int): Posición de la tarea que queremos eliminar.
+    '''     
     def eliminarTarea(self, posicion):
+        #Excepción en el caso de que el valor introducido no sea correcto nos de un mensaje de error.
         try:
-            del self.tareas[posicion]
+            del self.tareas[posicion-1]
+            print(Fore.RED+'Se ha eliminado la tarea con exito!!'+Style.RESET_ALL)
         except IndexError:
             print(Fore.RED+'La posición no existe.'+Style.RESET_ALL)
     
+    '''
+        Cambiamos el estado de tarea a completada(TRUE)
+        
+        Parámetros:
+            posición(int): Posición de la tarea que queremos completar en la lista.  
+    '''
     def completarTarea(self, posicion):
+        #Excepción en el caso de que el valor introducido no sea correcto nos de un mensaje de error.
         try:
-            self.tareas[posicion].tareaCompletada()
+            if self.tareas[posicion-1].completada:
+                print('La tarea que intentas completar ya fue completada anteriormente.')
+            else:
+                self.tareas[posicion-1].tareaCompletada()
+                print('Tarea completada con exito!!')
         except IndexError:
            print(Fore.RED+'La posición no existe.'+Style.RESET_ALL)
--- tareas/utils/menu.py
+++ tareas/utils/menu.py
@@ -15,7 +15,7 @@ class Menu:
         print('1. Mostrar tareas.')
         print('2. Agregar tarea.')
         print('3. Completar tarea.')
-        print('4. Mostrar tareas.')
+        print('4. Eliminar tarea.')
         print('5. Salir.\n')
         
     def seleccionarOpciones(self):
@@ -30,11 +30,19 @@ class Menu:
                 descripcion = input('Introduzca la tarea a realizar: ')
                 tarea.agregarTarea(descripcion)
             elif opcion == '3':
-                posicion = input(Fore.BLUE+'Introduzca el número de la tarea que quiere completar: '+Style.RESET_ALL)
-                tarea.completarTarea(posicion)
+                #Excepción en el caso de que el valor introducido no sea correcto nos de un mensaje de error.
+                try:
+                    posicion = int(input(Fore.BLUE+'Introduzca el número de la tarea que quiere completar: '+Style.RESET_ALL))
+                    tarea.completarTarea(posicion)
+                except ValueError:
+                    print(Fore.RED+'La posición no existe.'+Style.RESET_ALL)
             elif opcion == '4':
-                posicion = input(Fore.BLUE+'Introduzca el número de la tarea que quiere eliminar: '+Style.RESET_ALL)
-                tarea.eliminarTarea(posicion)
+                #Excepción en el caso de que el valor introducido no sea correcto nos de un mensaje de error.
+                try:
+                    posicion = int(input(Fore.BLUE+'Introduzca el número de la tarea que quiere eliminar: '+Style.RESET_ALL))
+                    tarea.eliminarTarea(posicion)
+                except ValueError:
+                    print(Fore.RED+'La posición no existe.'+Style.RESET_ALL)
             elif opcion == '5':
                 print('Cerrando programa.')
             else:
